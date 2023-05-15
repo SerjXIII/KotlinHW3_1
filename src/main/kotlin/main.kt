ba085ab9e5f414aff17a895e8a7f7517a005de21
Serj <krasnobor13@gmail.com>
2023-05-15 09:30:22 +0300

    rename const -> CONST

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 import kotlin.math.absoluteValue
 
-const val minute = 60
-const val hour = 60 * minute
-const val day = 24 * hour
+const val MINUTE = 60
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
 
 fun main() {
 
@@ -17,26 +17,26 @@ fun agoToText(time: Int): String {
 
     return when (time) {
         in 0..60 -> "только что"
-        in 61..hour -> "${time / 60} ${minutes(time)} назад"
-        in hour + 1..day -> "${time / hour} ${hours(time)} назад"
-        in day + 1..day * 2 -> "вчера"
-        in day * 2 + 1..day * 3 -> "позавчера"
+        in 61..HOUR -> "${time / 60} ${minutes(time)} назад"
+        in HOUR + 1..DAY -> "${time / HOUR} ${hours(time)} назад"
+        in DAY + 1..DAY * 2 -> "вчера"
+        in DAY * 2 + 1..DAY * 3 -> "позавчера"
         else -> "давно"
     }
 }
 
 fun minutes(time: Int): String {
     return when {
-        (time / minute) % 10 == 1 && (time / minute) != 11 -> "минуту"
-        (time / minute) % 10 in 2..4 && time / minute !in 12..14 -> "минуты"
+        (time / MINUTE) % 10 == 1 && (time / MINUTE) != 11 -> "минуту"
+        (time / MINUTE) % 10 in 2..4 && time / MINUTE !in 12..14 -> "минуты"
         else -> "минут"
     }
 }
 
 fun hours(time: Int): String {
     return when {
-        (time / hour) % 10 == 1 && (time / hour) != 11 -> "час"
-        (time / hour) % 10 in 2..4 && time / hour !in 12..14 -> "часа"
+        (time / HOUR) % 10 == 1 && (time / HOUR) != 11 -> "час"
+        (time / HOUR) % 10 in 2..4 && time / HOUR !in 12..14 -> "часа"
         else -> "часов"
     }
 }
